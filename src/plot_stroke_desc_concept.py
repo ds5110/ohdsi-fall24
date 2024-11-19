12a7c6577b5ac05937026fd76533a91388443cf9
Alexandra Corcoran <corcoran.al@northeastern.edu>
2024-11-19 11:58:42 -0500

    desc plot update

--- src/plot_stroke_desc_concept.py
+++ src/plot_stroke_desc_concept.py
@@ -29,15 +29,24 @@ print(
 df["condition_concept_id"] = df["condition_concept_id"].apply(str)
 
 # Plot.
-fig, ax = plt.subplots(2, 1, figsize=(12, 20))
-sns.histplot(data=df, x="condition_concept_id", ax=ax[0])
-sns.histplot(data=df, x="condition_concept_id", ax=ax[1])
+fig, ax = plt.subplots(2, 1, figsize=(12, 22))
+sns.histplot(data=df, x="condition_concept_id", color="#2292B5", legend=False, ax=ax[0])
+sns.histplot(data=df, x="condition_concept_id", color="#00246D", legend=False, ax=ax[1])
+
+#plot 1 - reg
 ax[0].tick_params(axis="x", rotation=90)
-ax[0].set_title("Descendant concept id of the stroke cohort")
-ax[0].set_xlabel("Condition concept id", labelpad=20)
+ax[0].set_title("Descendant Concept Id of the Stroke Cohort", weight="bold", pad=15)
+ax[0].set_xlabel("Condition Concept Id", fontsize=12, labelpad=20)
+ax[0].set_ylabel("Count", fontsize=12, labelpad=15)
+
+#plot 2 - log
+ax[1].set_title("Descendant Concept Id of the Stroke Cohort - Log", weight="bold", pad=15)
 ax[1].tick_params(axis="x", rotation=90)
-ax[1].set_xlabel("Condition concept id", labelpad=20)
+ax[1].set_xlabel("Condition Concept Id", fontsize=12, labelpad=20)
+ax[1].set_ylabel("Count", fontsize=12, labelpad=15)
 ax[1].set_yscale("log")
+
+#plot - all 
+plt.subplots_adjust(bottom=0.2, hspace=1)
 plt.savefig("figs/stroke_desc_concept_id.png", bbox_inches="tight")
-plt.tight_layout()
 plt.show()

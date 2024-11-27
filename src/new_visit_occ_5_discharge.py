ba21587095aab368f3bb06ce4734a8337f134ff8
Jaee <oh.jaee@northeastern.edu>
2024-11-27 11:35:10 -0500

    cleared unused src files

--- src/new_visit_occ_5_discharge.py
+++ src/new_visit_occ_5_discharge.py
@@ -25,25 +25,32 @@ WHERE v.visit_end_date > f.condition_start_date;
 df = read_df(con, query)
 
 # Ensure date columns are in datetime format
-df['visit_end_date'] = pd.to_datetime(df['visit_end_date'])
-df['condition_start_date'] = pd.to_datetime(df['condition_start_date'])
+df["visit_end_date"] = pd.to_datetime(df["visit_end_date"])
+df["condition_start_date"] = pd.to_datetime(df["condition_start_date"])
 
 # Filter rows where visit_end_date is within 180 days of condition_start_date
-df = df[(df['visit_end_date'] > df['condition_start_date']) &
-        (df['visit_end_date'] <= df['condition_start_date'] + pd.Timedelta(days=180))]
+df = df[
+    (df["visit_end_date"] > df["condition_start_date"])
+    & (df["visit_end_date"] <= df["condition_start_date"] + pd.Timedelta(days=180))
+]
 
 # Filter rows where discharge_to_concept_id is in total_id
 valid_concept_ids = set(total_id.keys())
-df = df[df['discharge_to_concept_id'].isin(valid_concept_ids)]
+df = df[df["discharge_to_concept_id"].isin(valid_concept_ids)]
 
 # Map discharge_to_concept_id to its label using total_id
-df['discharge_to_concept_id'] = df['discharge_to_concept_id'].map(total_id)
+df["discharge_to_concept_id"] = df["discharge_to_concept_id"].map(total_id)
 
 # Add a row number per person_id to distinguish multiple pairs
 df["pair_number"] = df.groupby("person_id").cumcount() + 1
 
 # Pivot the table to create columns for each pair, ensuring desired column order
-pivot_columns = ["visit_start_date", "visit_source_concept_id", "visit_end_date", "discharge_to_concept_id"]
+pivot_columns = [
+    "visit_start_date",
+    "visit_source_concept_id",
+    "visit_end_date",
+    "discharge_to_concept_id",
+]
 df_wide = df.pivot(index="person_id", columns="pair_number", values=pivot_columns)
 
 # Flatten the multi-level columns created by pivot
@@ -63,7 +70,7 @@ df_wide = df_wide.reset_index()
 df_wide.info()
 
 # Save the DataFrame to a CSV file
-output_file = 'visit_oc_all_discharge.csv'
-df_wide.to_csv(output_file, index=False)
+output_file = "visit_oc_all_discharge.csv"
+# df_wide.to_csv(output_file, index=False)
 
-print(f"df_wide has been saved to {output_file}")
+# print(f"df_wide has been saved to {output_file}")

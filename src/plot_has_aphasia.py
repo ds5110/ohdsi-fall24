65e6dd4506dbd504deb28a0bc14f6ba4a83c6feb
Alexandra Corcoran <corcoran.al@northeastern.edu>
2024-12-04 11:42:13 -0500

    sm plot edit

--- src/plot_has_aphasia.py
+++ src/plot_has_aphasia.py
@@ -22,7 +22,7 @@ df["has_aphasia"] = df["has_aphasia"].apply(str)
 
 # Plot.
 fig, ax = plt.subplots()
-sns.histplot(data=df, x="has_aphasia", hue="has_aphasia", palette='ocean', shrink=.9)
+sns.histplot(data=df, x="has_aphasia", hue="has_aphasia", palette='ocean', shrink=.9, discrete=True)
 ax.set_xticklabels(["No Aphasia", "Has Aphasia"], fontsize=9)
 ax.set_title("Stroke Cohort: Aphasia", fontsize=14, weight="bold", pad=20)
 plt.xlabel("Condition Presence Among Users", fontsize=12, labelpad=15)
